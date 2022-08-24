f6fe3de894de19c2271bc6b11ed38f1dac154314
masoodehghan <masood8204@gmail.com>
2022-08-24 13:14:21 +0430

    modify docker image for app service

--- src/thread/urls.py
+++ src/thread/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('answer/create/', views.AnswerCreateView.as_view(), name='answer-create'),
     path('answer/<int:pk>/', views.AnswerDetailView.as_view(), name='answer-detail'),
 
-    path('tag/<slug:slug>/', views.QuestionListByTagView.as_view(), name='question-list-by-tag'),
+    path('tag/<slug:slug>/questions/', views.QuestionListByTagView.as_view(), name='question-list-by-tag'),
 
     path('vote/create/', views.VoteView.as_view(), name='vote'),
     path('leaderboard/', views.LeaderboardView.as_view(), name='leaderboard')
